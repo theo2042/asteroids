9cad1b4385b6a22669b50fc319923f7c7df7d281
theo2042 <theo2042@gmail.com>
2025-12-12 13:05:39 +0900

    CH2 L3 completed

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     
@@ -22,6 +24,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        
 
 
 
