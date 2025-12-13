2cd237d5e9f44cf9d5e7e79e7ca316a48b4d3c4f
theo2042 <theo2042@gmail.com>
2025-12-13 13:51:34 +0900

    CH3 L2 - step 4 done

---
+++ player.py
@@ -0,0 +1,26 @@
+from constants import PLAYER_RADIUS
+import pygame
+class Player(CircleShape):
+    def __init__(self,x,y,PLAYER_RADIUS):
+        self.x = x
+        self.y = y        
+        self.rotation = 0
+
+        # in the Player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    
+
+    
+
+    
+    
+
+
+
+
